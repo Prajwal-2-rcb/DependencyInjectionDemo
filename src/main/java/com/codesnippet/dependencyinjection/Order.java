499658521c36db6f312e8e9c089896323692e2a3
Prajwal M V <mvprajwal546@gmail.com>
2026-01-10 19:46:33 +0530

    Dependency Injection  Lazy Annotation

--- src/main/java/com/codesnippet/dependencyinjection/Order.java
+++ src/main/java/com/codesnippet/dependencyinjection/Order.java
@@ -1,6 +1,25 @@
 package com.codesnippet.dependencyinjection;
 
-public interface Order {
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Order {
 
+    @Autowired
+    @Lazy
+    User user;
+
+    public void process() {
+        System.out.println("Order process");
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        Order order = new Order();
+        OnlineOrder onlineOrder = new OnlineOrder();
+        user.setOrder(onlineOrder);
+    }
 }
